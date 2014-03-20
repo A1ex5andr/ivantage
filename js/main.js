b822e81f75fbfe53b1d7239ec3a8b50b2c3b626b
A1ex5andr <sashayushchak@gmail.com>
2014-03-20 14:12:46 +0200

    First Page

---
+++ js/main.js
@@ -0,0 +1,39 @@
+jQuery(document).ready(function($) {
+
+    // button hover more/less toggle
+    $('.navi-more ').hover(
+        function() {
+            $('.navi-more h2 storng').text('Less');
+        }, function() {
+            $('.navi-more h2 storng').text('More');
+        }
+    );
+
+    // advanced search show/hide
+    $('.advSearch-btn').click(function() {
+        $('.advSearch-container').slideToggle('display');
+    })
+
+    //custom arrow for select drop list
+    $(function() {
+        $("#formList").selectbox();
+    });
+
+    //popup controls show/hide
+    // function hidePopUp() {
+    //     $('#popUp').addClass('hide');
+    // };
+    // hidePopUp();
+
+    $('.head-logo').click(function() {
+        //$('#popUp').removeClass('hide');
+        $('.popUp').addClass('show');
+
+    });
+
+    $('.cntrl-cancel').click(function() {
+        $('.popUp').removeClass('show');
+    });
+
+
+});
