d0b876cc7cbcb4573be1328dcec6a884500a05dc
A1ex5andr <sashayushchak@gmail.com>
2014-03-22 10:52:19 +0200

    min fixes & AdvSearch Menu JS

--- js/main.js
+++ js/main.js
@@ -9,12 +9,37 @@ jQuery(document).ready(function($) {
         }
     );
 
-    // advanced search show/hide
-    $('.advSearch-btn').click(function() {
-        $('.advSearch-container').slideToggle('display');
-    })
+    // advanced search show/hide and buttons callBack
+    var container = $(".advSearch-container");
+    // Bind the link to toggle the slide.
+    var slideAdvancedSearch = function() {
+        // Toggle the slide based on its visibility
+        if (container.is(":visible")) {
+            // Hide - slide up.
+            container.slideUp(1000);
+            $('.canc').slideToggle('fast');
+            $('.srch').slideToggle('fast');
+        } else {
+            // Show - slide down.
+            container.slideDown(1000, function() {
+                $('.canc').slideToggle('fast');
+                $('.srch').slideToggle('fast');
+            });
+        }
+    };
+
+    //run slide on AdvSearch button
+    $(".advSearch-btn").click(function() {
+        slideAdvancedSearch();
+    });
+    //run slide on Cancel button
+    $('.canc').click(function(event) {
+        slideAdvancedSearch();
+    });
+
 
     //custom arrow for select drop list
+    $('#formList').customSelect();
 
 
     //popup controls show/hide
